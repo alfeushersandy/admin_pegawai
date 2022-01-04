e3873e00a45650a492de6cbd236ce47f367bdb91
alfeushersandy <alfeusputra@gmail.com>
2022-01-04 15:24:41 +0700

    mengganti field keterangan

--- app/Models/sop.php
+++ app/Models/sop.php
@@ -10,7 +10,7 @@ class sop extends Model
     use HasFactory;
     protected $fillable = [
         'Nama_berkas',
-        'keterangan',
+        'keterangan_berkas',
         'uploaded_file'
     ];
 }
